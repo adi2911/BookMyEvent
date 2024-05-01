984036229746bd2b1f61d98d722ffdfbfaeb5dd7
adjohari <adjohari25@gmail.com>
2024-05-01 18:39:24 +0530

    add signin, signout and headers component

--- Client/pages/_app.js
+++ Client/pages/_app.js
@@ -1,5 +1,28 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Headers from "../components/headers";
 
-export default ({Component, pageProps}) =>{
-    return <Component {...pageProps}/>
-}
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Headers currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+/*
+getInitialProps doesn't work directly at more than two places. To make it work in multiple place we have manually call it in th first call.
+*/
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx); //appContext is different from index context
+  const { data } = await client.get("/api/users/currentuser");
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
--- Client/pages/index.js
+++ Client/pages/index.js
@@ -1,16 +1,19 @@
 import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser);
-   
-    return <h1>{ currentUser ? "You are signed in" : "You are not signed in" }</h1>;
+  console.log(currentUser);
+
+  return <h1>{currentUser ? "You are signed in" : "You are not signed in"}</h1>;
 };
 /*server rendering of getInitialProps gives error if we directly hit /api/users/currentuser due to k8s container
  we need to request to the url through ingnress-nginx, keeping the cookie information.
 */
-LandingPage.getInitialProps = async(context)=>{
-    const client = buildClient(context);
-   const {data} = await client.get('/api/users/currentuser')
-   return data
-}
+/*
+getInitialProps doesn't work directly at more than two places. To make it work in multiple place we have manually call it in th first call
+ */
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
 
-export default LandingPage
+export default LandingPage;
